270cfaf7fe291b74f5c0fee84353757270411ae1
Daria <d.s.mudrova@student.khai.edu>
2023-10-22 17:22:53 +0300

    Zoo 1-st stage

---
+++ Zoo/zoo.py
@@ -0,0 +1,11 @@
+"""ZooBot: displays simple notifications,displays the animal camel,creates a list of animals and displays them by number"""
+
+#Виводимо повідомлення
+print("I love animals!")
+
+print("Let's check out the animals...")
+
+print("The deer looks fine.")
+
+print("The lion looks healthy.")
+
