48eb13c5f4d88600a46edd3231abfe1e2f58fe67
Daria <d.s.mudrova@student.khai.edu>
2023-10-26 18:57:38 +0300

    Zoo 3-rd stage

--- Zoo/zoo.py
+++ Zoo/zoo.py
@@ -1,6 +1,4 @@
-"""ZooBot: displays simple notifications,displays the animal camel,creates a list of animals and displays them by number"""
 
-#Виводимо повідомлення
 print("I love animals!")
 
 print("Let's check out the animals...")
@@ -8,6 +6,10 @@ print("Let's check out the animals...")
 print("The deer looks fine.")
 
 print("The lion looks healthy.")
+
+
+print("Please enter the number of the habitat you would like to view\n(camel = 1, lion = 2, deer = 3, goose = 4, bat = 5,  rabbit = 6):")
+
 
 camel = r"""
 The camel habitat...
@@ -30,3 +32,126 @@ The camel habitat...
            ;,,      !,!   !,!     ;,;
            /_I      L_I   L_I     /_I
 Look at that!"""
+
+lion = r"""
+The lion habitat...
+
+                                               ,w.
+                                             ,YWMMw ,M ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW" @\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+ "^MP__.-'   ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \ :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is roaring!"""
+
+deer = r""" 
+The deer habitat... 
+    /| |\
+ `__\\ //__'
+    || ||
+ \__`\ |'__/
+  `_\\ //_'
+ _.,:---;,._
+ \_:     :_/
+   |@. .@|
+   |     |
+   ,\.-./ \
+   ;;`-'   `---__________-----.-.
+   ;;;                         \_\
+   ';;;                         |
+    ;    |                      ;
+     \    \    \        |      /
+      \_,  \   /        \     |\
+        |'; |  |,,,,,,,,/ \   \ \_
+        |   |  |           \  /   |
+         \  \  |           | / \  |
+          | || |           | |  | |
+          | || |           | |  | | 
+          | || |           | |  | |
+          | || |           | |  | |
+          |_||_|           |_|  |_|
+Pretty good! 
+--- 
+You've reached the end of the program."""
+
+goose = r""" 
+The goose habitat... 
+                              _
+                         ,- "" "".
+                       , '   ____  `.
+                     , ' ,'      `. `._
+  (`.     _..--.._ ,'  ,'          \   \
+ (`-.\  .-""     ""'  /            ( d _b
+(`._ ` -"" ,._        (             `-(  \
+<_ `     ( <`<        \                `-._\
+ <`-      (__< <       :
+  (__      (_<_<       ;
+    `------------------------------------------               
+Beautiful! 
+--- 
+You've reached the end of the program."""
+
+bat = r""" 
+The bat habitat... 
+_____________            _____________
+~-.           \ |\___/| /          .-~ 
+  ~-.         \ / o o \ /       .-~ 
+      >        \\  W  //        < 
+     /          /~---~\         \ 
+    /_         |        |       _\ 
+      ~-.      |        |    .-~ 
+         ;      \      /     i 
+        /___    /\    /\    ___\ 
+           ~-. /  \_/   \ .-~ 
+              V           V 
+It's doing fine. 
+--- 
+You've reached the end of the program."""
+
+rabbit = r""" 
+The rabbit habitat... 
+           , 
+          /|     __ 
+         / |   ,-~ / 
+        Y :|  // / 
+       | jj /( .^ 
+       >-"~"-v" 
+       /        Y 
+      jo o      | 
+     ( ~T~     j 
+      >._-'  _./ 
+     /  "~"    | 
+    Y      _,  | 
+  /|  ;-"~ _   l 
+/ l/  ,-"~      \ 
+\//\/         .- \ 
+ Y         /      Y 
+ l         I      ! 
+ ]\         _\   /"\ 
+(" ~----( ~   Y.    ) 
+It looks fine! 
+--- 
+You've reached the end of the program."""
+
+while True:
+    name = input("выбирите номер или напишите exit: ")
+    if name == '1':
+        print(camel)
+    elif name == '2':
+        print(lion)
+    elif name == '3':
+        print(deer)
+    elif name == '4':
+        print(goose)
+    elif name == '5':
+        print(bat)
+    elif name == '6':
+        print(rabbit)
