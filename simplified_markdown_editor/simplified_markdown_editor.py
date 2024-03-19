989a2cae5ac38e889e2089133488728aba067fea
Daria <d.s.mudrova@student.khai.edu>
2024-03-20 00:44:12 +0200

    Stage-2

--- simplified_markdown_editor/simplified_markdown_editor.py
+++ simplified_markdown_editor/simplified_markdown_editor.py
@@ -3,8 +3,9 @@ import os
 import validators
 
 def print_help():
-    print("Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line")
+    print("Available formatters: plain bold italic link inline-code header line-break ordered-list unordered-list")
     print("Special commands: !help !done")
+
 
 def format_text(formatter, text):
     if formatter == "plain":
@@ -14,12 +15,7 @@ def format_text(formatter, text):
     elif formatter == "italic":
         return f"*{text}*"
     elif formatter == "header":
-        level = int(input("Level: "))
-        if 1 <= level <= 6:
-            return f"{'#' * level} {text}\n"
-        else:
-            print("The level should be within the range of 1 to 6.")
-            return ""
+        return f"#{text}\n\n"
     elif formatter == "link":
         label = input("Label: ")
         url = input("URL: ")
@@ -31,22 +27,27 @@ def format_text(formatter, text):
     elif formatter == "inline-code":
         return f"`{text}`"
     elif formatter in ["ordered-list", "unordered-list"]:
-        num_items = int(input("Number of rows: "))
-        if num_items > 0:
-            formatted_text = ""
-            for i in range(num_items):
-                item_text = input(f"Row #{i+1}: ")
-                formatted_text += f"{i+1}. " if formatter == "ordered-list" else "* "
-                formatted_text += f"{item_text}\n"
-            return formatted_text
-        else:
-            print("The number of rows should be greater than zero")
+        try:
+            num_items = int(input("Number of rows: "))
+            if num_items > 0:
+                formatted_text = ""
+                for i in range(num_items):
+                    item_text = input(f"Row #{i+1}: ")
+                    formatted_text += f"{i+1}. " if formatter == "ordered-list" else "* "
+                    formatted_text += f"{item_text}\n"
+                return formatted_text
+            else:
+                print("The number of rows should be greater than zero")
+                return ""
+        except ValueError:
+            print("Invalid input. Number of rows should be a number.")
             return ""
-    elif formatter == "new-line":
+    elif formatter == "line-break":
         return "\n"
     else:
         print("Unknown formatting type or command")
         return ""
+
 
 def save_to_file(text):
     if text:
@@ -59,6 +60,7 @@ def save_to_file(text):
     else:
         print("Nothing to save.")
 
+
 def markdown_formatter():
     formatted_text = ""
     while True:
@@ -69,9 +71,10 @@ def markdown_formatter():
         elif user_input == "!done":
             save_to_file(formatted_text)
             break
-        elif user_input in ["plain", "bold", "italic", "header", "link", "inline-code", "ordered-list", "unordered-list", "new-line"]:
-            if user_input == "new-line":
-                formatted_text += format_text(user_input, "")
+        elif user_input in ["plain", "bold", "italic", "header", "link", "inline-code", "ordered-list", "unordered-list", "line-break"]:
+            if user_input == "header" or user_input == "line-break":
+                text = input("Text: ")
+                formatted_text += format_text(user_input, text)
             else:
                 text = input("Text: ")
                 formatted_text += format_text(user_input, text)
@@ -80,5 +83,7 @@ def markdown_formatter():
         else:
             print("Unknown formatting type or command")
 
+
 if __name__ == "__main__":
     markdown_formatter()
+
